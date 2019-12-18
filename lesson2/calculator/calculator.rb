f91f1c1d2be18de7f890f63938e9c9b1576b0a91
mike lederle <mrlederle@gmail.com>
2019-12-18 11:18:55 -0500

    (rubocop) shorten long lines (>80) and assign result to if statement

--- lesson2/calculator/calculator.rb
+++ lesson2/calculator/calculator.rb
@@ -3,17 +3,18 @@ puts "What is your first number:"
 num1 = gets.chomp
 puts "What is your second number:"
 num2 = gets.chomp
-puts "What is the operation you wish to perform: (add)ition, (sub)traction, (mult)iplication or (div)ision"
+puts "What is the operation you wish to perform: " \
+     "(add)ition, (sub)traction, (mult)iplication or (div)ision"
 op = gets.chomp
 
-if op == 'add'
-  result = num1.to_i + num2.to_i
-elsif op == 'sub'
-  result = num1.to_i - num2.to_i
-elsif op == 'mult'
-  result = num1.to_i * num2.to_i
-else
-  result = num1.to_f / num2.to_f
-end
+result = if op == 'add'
+           num1.to_i + num2.to_i
+         elsif op == 'sub'
+           num1.to_i - num2.to_i
+         elsif op == 'mult'
+           num1.to_i * num2.to_i
+         else
+           num1.to_f / num2.to_f
+         end
 
 puts result
