e93e39fd451537f2be4685c987f9694f47b87c55
mike lederle <mrlederle@gmail.com>
2020-05-11 16:12:37 -0400

    Canary test for Calculator
    Instrumenting test capablility for Calculator. Calculator
    has a compute function that does all the work of reading,
    writing and computing.  Eventually there will be  helper
    classes Reader and Writer that are injected into Calculator,
    this allows the helpers to be tested independently and to
    relieve the input output responsibility from Calculator.
    Injecting these classes also allows mocks to be used when we
    test the overall behavior (like integration tests), and this
    is what is set up here.
    
    See this blog post for the details.
    [Testing code that's hard to test](https://8thlight.com/blog/josh-cheek/2011/10/01/testing-code-thats-hard-to-test.html)

--- lesson2/calculator/calculator.rb
+++ lesson2/calculator/calculator.rb
@@ -1,20 +1,47 @@
-puts "Welcome to the Calculator!"
-puts "What is your first number:"
-num1 = gets.chomp
-puts "What is your second number:"
-num2 = gets.chomp
-puts "What is the operation you wish to perform: " \
-     "(add)ition, (sub)traction, (mult)iplication or (div)ision"
-op = gets.chomp
+# puts "Welcome to the Calculator!"
+# puts "What is your first number:"
+# num1 = gets.chomp
+# puts "What is your second number:"
+# num2 = gets.chomp
+# puts "What is the operation you wish to perform: " \
+#      "(add)ition, (sub)traction, (mult)iplication or (div)ision"
+# op = gets.chomp
+# 
+# result = if op == 'add'
+#            num1.to_i + num2.to_i
+#          elsif op == 'sub'
+#            num1.to_i - num2.to_i
+#          elsif op == 'mult'
+#            num1.to_i * num2.to_i
+#          else
+#            num1.to_f / num2.to_f
+#          end
+# 
+# puts result
 
-result = if op == 'add'
-           num1.to_i + num2.to_i
-         elsif op == 'sub'
-           num1.to_i - num2.to_i
-         elsif op == 'mult'
-           num1.to_i * num2.to_i
-         else
-           num1.to_f / num2.to_f
-         end
+class Calculator
+  def self.compute(reader, writer)
+  end
+end
 
-puts result
+require 'minitest/autorun'
+
+class CalculatorTest < Minitest::Test
+  def setup
+    @reader = MockReader.new
+    @writer = MockWriter.new
+  end
+
+  def test_happy
+    @reader.responses = [1, 2, '1']
+    assert_equal 3, Calculator.compute(@reader, @writer)
+  end
+
+  class MockReader
+    attr_accessor :responses
+  end
+
+  class MockWriter
+
+  end
+end
