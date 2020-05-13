377c0cb6bad8a9293ed07e986287be5584815526
mike lederle <mrlederle@gmail.com>
2020-05-12 23:30:46 -0400

    Add micro readme to top of file
    Just left a note to myself about what this brnahc is attempting
    to accomplish. Also deleted the original procedural code that was
    commented out and basically only in the way.

--- lesson2/calculator/calculator.rb
+++ lesson2/calculator/calculator.rb
@@ -1,23 +1,6 @@
-# puts "Welcome to the Calculator!"
-# puts "What is your first number:"
-# num1 = gets.chomp
-# puts "What is your second number:"
-# num2 = gets.chomp
-# puts "What is the operation you wish to perform: " \
-#      "(add)ition, (sub)traction, (mult)iplication or (div)ision"
-# op = gets.chomp
-# 
-# result = if op == 'add'
-#            num1.to_i + num2.to_i
-#          elsif op == 'sub'
-#            num1.to_i - num2.to_i
-#          elsif op == 'mult'
-#            num1.to_i * num2.to_i
-#          else
-#            num1.to_f / num2.to_f
-#          end
-# 
-# puts result
+# no-class-test branch
+# try to remove as much class machinery as possible
+# i.e., conform to LS pedagogy at this point
 
 class Calculator
   def self.compute(reader = Reader, writer = Writer)
