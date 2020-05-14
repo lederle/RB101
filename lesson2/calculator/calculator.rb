23b17b86c7a82f69def8b629337c34828856bbb3
mike lederle <mrlederle@gmail.com>
2020-05-14 11:35:15 -0400

    Fix Rubocop warnings
    The diff should indicate what was done pretty clearly
    so I won't elaborate here. There are detailed
    comments added for each class, mostly explaining
    how everything fits vis-a-vis testing.
    
    Rubocop version 0.82.0

--- lesson2/calculator/calculator.rb
+++ lesson2/calculator/calculator.rb
@@ -1,6 +1,4 @@
-# no-class-test branch
-# try to remove as much class machinery as possible
-# i.e., conform to LS pedagogy at this point
+# frozen_string_literal: true
 
 def calculator(reader = Reader, writer = Writer)
   writer.display_banner
@@ -32,36 +30,51 @@ def calculate(num1, num2, oper)
   num1.send(oper, num2)
 end
 
+# Reader is a helper class for the calculator
+# function. The default parameter is $stdin
+# but its main reason to be is to allow a
+# fake reader to by substituted during testing.
+# It performs the gets operations for the
+# calculator.
 class Reader
-  def self.read_num(is = $stdin)
-    is.gets.chomp
+  def self.read_num(i_stream = $stdin)
+    i_stream.gets.chomp
   end
 
-  def self.read_op(is = $stdin)
-    is.gets.chomp
+  def self.read_op(i_stream = $stdin)
+    i_stream.gets.chomp
   end
 end
 
+# Writer is a helper class for the calculator
+# function. The default parameter is $stdout
+# but its main reason to be is to allow a
+# fake writer to by substituted during testing.
+# It performs the puts (and related) operations
+# for calculator.
 class Writer
-  def self.display_banner(os = $stdout)
-    os.puts 'Welcome to Calculator!'
+  def self.display_banner(o_stream = $stdout)
+    o_stream.puts 'Welcome to Calculator!'
   end
 
-  def self.ask_for_number(os = $stdout, term)
-    os.puts "What's the #{term.to_s} number?"
+  def self.ask_for_number(o_stream = $stdout, term)
+    # Note to self: symbol auto converts to string
+    o_stream.puts "What's the #{term} number?"
   end
 
-  def self.ask_for_operation(os = $stdout)
-    os.puts "What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide"
+  def self.ask_for_operation(o_stream = $stdout)
+    o_stream.puts 'What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide'
   end
 
-  def self.display_result(os = $stdout, res)
-    os.puts "The result is #{res}"
+  def self.display_result(o_stream = $stdout, res)
+    o_stream.puts "The result is #{res}"
     res
   end
 end
 
 require 'minitest/autorun'
+# Test the Reader; use a StringIO
+# to fake $stdin
 class ReaderTest < Minitest::Test
   def read_num(input)
     @stream = StringIO.new input
@@ -86,6 +99,12 @@ class ReaderTest < Minitest::Test
   end
 end
 
+# Test the Writer; use a StringIO
+# to fake $stdout. Much of the goings-on
+# here seem useless, or simply testing
+# the IO functionality, but if you testing
+# first, theses tests form a "are the messages
+# as wanted" spec of sorts.
 class WriterTest < Minitest::Test
   def setup
     @stream = StringIO.new
@@ -95,7 +114,7 @@ class WriterTest < Minitest::Test
   def test_display_banner
     @writer.display_banner @stream
     @stream.rewind
-    expected =<<~OUT
+    expected = <<~OUT
       Welcome to Calculator!
     OUT
     assert_equal expected, @stream.read
@@ -104,7 +123,7 @@ class WriterTest < Minitest::Test
   def test_ask_for_first_number
     @writer.ask_for_number @stream, :first
     @stream.rewind
-    expected =<<~OUT
+    expected = <<~OUT
       What's the first number?
     OUT
     assert_equal expected, @stream.read
@@ -113,7 +132,7 @@ class WriterTest < Minitest::Test
   def test_ask_for_second_number
     @writer.ask_for_number @stream, :second
     @stream.rewind
-    expected =<<~OUT
+    expected = <<~OUT
       What's the second number?
     OUT
     assert_equal expected, @stream.read
@@ -122,7 +141,7 @@ class WriterTest < Minitest::Test
   def test_ask_for_operation
     @writer.ask_for_operation @stream
     @stream.rewind
-    expected =<<~OUT
+    expected = <<~OUT
       What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide
     OUT
     assert_equal expected, @stream.read
@@ -132,7 +151,7 @@ class WriterTest < Minitest::Test
     res = 22
     out = @writer.display_result @stream, res
     @stream.rewind
-    expected =<<~OUT
+    expected = <<~OUT
       The result is 22
     OUT
     assert_equal expected, @stream.read
@@ -140,6 +159,15 @@ class WriterTest < Minitest::Test
   end
 end
 
+# Test the calculator; I consider these
+# integration type tests, where the
+# overall behavior of the calculator
+# is what is interesting, not the line
+# to line IO (ReaderTest and WriterTest
+# do that). DIY Mocks are used (I am
+# still a little confused by Minitest::Mocks
+# honestly), we are mostly interested
+# in the return value from calculator.
 class CalculatorTest < Minitest::Test
   def setup
     @reader = MockReader.new
@@ -147,27 +175,27 @@ class CalculatorTest < Minitest::Test
   end
 
   def test_happy
-    @reader.responses = %w[1, 2, 1]
+    @reader.responses = %w[1 2 1]
     assert_equal 3, calculator(@reader, @writer)
   end
 
   def test_subtract
-    @reader.responses = %w[23, 12, 2]
+    @reader.responses = %w[23 12 2]
     assert_equal 11, calculator(@reader, @writer)
   end
 
   def test_product
-    @reader.responses = %w[23, 2, 3]
+    @reader.responses = %w[23 2 3]
     assert_equal 46, calculator(@reader, @writer)
   end
 
   def test_quotient
-    @reader.responses = %w[23, 2, 4]
+    @reader.responses = %w[23 2 4]
     assert_equal 11.5, calculator(@reader, @writer)
   end
 
   def test_noop
-    @reader.responses = %w[23, 2, 999]
+    @reader.responses = %w[23 2 999]
     assert_nil calculator(@reader, @writer)
   end
 
@@ -178,6 +206,10 @@ class CalculatorTest < Minitest::Test
     assert_equal 1.0, calculate('1', '1', :/)
   end
 
+  # Fake Reader class. To mimic a stream
+  # of input from user, an array is used
+  # and individual inputs are shifted off
+  # for use in the test.
   class MockReader
     attr_accessor :responses
 
@@ -190,17 +222,19 @@ class CalculatorTest < Minitest::Test
     end
   end
 
+  # Fake Writer class. We don't want to
+  # write anything, just test end-to-end
+  # behavior, so most of these are empty
+  # methods. The display_results method
+  # does need to return the result.
   class MockWriter
-    def display_banner
-    end
+    def display_banner; end
 
-    def ask_for_number(term)
-    end
+    def ask_for_number(term); end
 
-    def ask_for_operation
-    end
+    def ask_for_operation; end
 
-    def display_result res
+    def display_result(res)
       res
     end
   end
