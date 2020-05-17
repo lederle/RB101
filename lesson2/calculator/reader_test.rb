0bd31e6bb416eed466e3a9cc6b5143bfa696cb93
mike lederle <mrlederle@gmail.com>
2020-05-17 18:54:14 -0400

    Add newlines to ReaderTest; refactor test names
    I want to start testing all input with newlines attached,
    since a bare newline signals nothing entered, which is an
    important input. Also renamed tests to be more consistent
    with each other.

--- lesson2/calculator/reader_test.rb
+++ lesson2/calculator/reader_test.rb
@@ -19,40 +19,48 @@ class ReaderTest < Minitest::Test
     Reader.read_new_calc @stream
   end
 
-  def test_read_a_number
-    assert_equal '10', read_num('10')
+  def test_read_number_given_a_number
+    assert_equal '10', read_num("10\n")
   end
 
-  def test_read_not_a_number
-    refute read_num('m')
+  def test_read_number_given_not_a_number
+    refute read_num("k\n")
   end
 
-  def test_read_not_a_number_check_not_nil
-    refute_nil read_num('m')
+  def test_read_number_given_newline
+    refute read_num("\n")
   end
 
-  def test_zero
-    refute read_num('0')
+  def test_read_number_check_not_nil
+    refute_nil read_num("k\n")
   end
 
-  def test_read_an_op
-    assert_equal '1', read_op('1')
+  def test_read_number_given_zero
+    refute read_num("0\n")
   end
 
-  def test_read_a_no_op
-    assert_equal '11', read_op('11')
+  def test_read_op_given_valid_op
+    assert_equal '1', read_op("1\n")
   end
 
-  def test_read_new_calc_yes
-    assert_equal 'y', read_new_calc('y')
+  def test_read_op_given_invalid_op
+    assert_equal '11', read_op("11\n")
   end
 
-  def test_read_new_calc_capital_y
-    assert_equal 'y', read_new_calc('Y')
+  def test_read_new_calc_given_yes
+    assert_equal 'y', read_new_calc("y\n")
   end
 
-  def test_read_new_calc_no
-    assert_equal 'n', read_new_calc('n')
+  def test_read_new_calc_given_capital_y
+    assert_equal 'y', read_new_calc("Y\n")
+  end
+
+  def test_read_new_calc_given_not_yes
+    assert_equal 'n', read_new_calc("n\n")
+  end
+
+  def test_read_new_calc_given_newline
+    assert_equal '', read_new_calc("\n")
   end
 end
 
